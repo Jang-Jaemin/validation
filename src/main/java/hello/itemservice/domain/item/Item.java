eda8109116458a77264b822916ce3e42ebeac800
admin <wkwovlf12@naver.com>
2022-08-31 13:41:35 +0900

    22.8.31

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -34,3 +34,11 @@ public class Item {
         this.quantity = quantity;
     }
 }
+
+//  검증애노테이션
+//  @NotBlank: 빈값 + 공백만있는경우를허용하지않는다.
+//  @NotNull: null을허용하지않는다.
+//  @Range(min = 1000, max = 1000000): 범위안의값이어야한다.
+//  @Max(9999): 최대 9999까지만허용한다.
+
+//
--- src/test/java/hello/itemservice/validation/BeanValidationTest.java
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -13,6 +13,9 @@ public class BeanValidationTest {
 
     @Test
     void beanValidation() {
+        //  검증기생성
+        //  다음코드와같이검증기를생성한다.
+        //  이후스프링과통합하면우리가직접이런코드를작성하지는 않으므로, 이렇게사용하는구나정도만참고하자
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
 
@@ -21,6 +24,11 @@ public class BeanValidationTest {
         item.setPrice(0);
         item.setQuantity(10000);
 
+
+        //  검증실행
+        //  검증대상( item)을직접검증기에넣고그결과를받는다.
+        //  Set에는ConstraintViolation이라는검증 오류가담긴다.
+        //  따라서결과가비어있으면검증오류가없는것이다.
         Set<ConstraintViolation<Item>> violations = validator.validate(item);
         for (ConstraintViolation<Item> violation : violations) {
             System.out.println("violation = " + violation);
