f00c69b5e0936414a2a6f3cca60f721e7be7f518
admin <wkwovlf12@naver.com>
2022-09-01 23:26:46 +0900

    22.9.1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -141,3 +141,41 @@ public class ValidationItemControllerV3 {
 
 }
 
+//  Bean Validation - 에러코드
+//  Bean Validation이 기본으로 제공하는 오류메시지를 좀 더 자세히 변경하고 싶으면 어떻게하면 될까?
+//  Bean Validation을 적용하고 bindingResult에 등록된 검증 오류 코드를 보자.
+//  오류코드가 애노테이션 이름으로 등록된다.
+//  마치typeMismatch와 유사하다.
+//  NotBlank라는 오류코드를 기반으로 MessageCodesResolver를 통해 다양한 메시지코드가 순서대로 생성된다.
+
+//  ex)
+//  @NotBlank 아래 순서대로 진행된다.
+//  NotBlank.item.itemName
+//  NotBlank.itemName
+//  NotBlank.java.lang.String
+//  NotBlank
+
+//  @Range
+//  Range.item.price
+//  Range.price
+//  Range.java.lang.Integer
+//  Range
+
+//  Bean Validation - 오브젝트오류
+//  Bean Validation에서 특정필드( FieldError)가 아닌 해당 오브젝트 관련 오류( ObjectError)는 어떻게 처리할 수 있을까?
+//  다음과 같이 @ScriptAssert()를 사용하면 된다.
+
+//  ex)
+//  @Data
+//  @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
+//  public class Item {
+//  //...
+//  }
+
+//  메시지 코드
+//  ScriptAssert.
+//  item ScriptAssert
+//  그런데실제사용해보면제약이많고복잡하다.
+//  그리고실무에서는검증기능이해당객체의범위를 넘어서는경우들도종종등장하는데, 그런경우대응이어렵다.
+//  따라서 오브 젝트 오류(글로벌오류)의 경우 @ScriptAssert을 억지로 사용하는것 보다는
+//  다음과같이 오브젝트 오류 관련 부분만 직접 자바 코드로 작성하는것을 권장한다.
